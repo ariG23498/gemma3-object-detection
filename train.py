ab3da50464df89f2dca9983834479837e57876a9
Aditi Bindal <aditii.b6@gmail.com>
2025-05-29 01:55:03 +0530

    feat(train, predict): generalize model loading to test other VLMs

--- train.py
+++ train.py
@@ -5,12 +5,13 @@ from functools import partial
 import torch
 from datasets import load_dataset
 from torch.utils.data import DataLoader
-from transformers import AutoProcessor, Gemma3ForConditionalGeneration
+from transformers import AutoProcessor, AutoModelForCausalLM, AutoModelForSeq2SeqLM, BlipForConditionalGeneration, Gemma3ForConditionalGeneration
 
 from config import Configuration
 from utils import train_collate_function
 
 import albumentations as A
+import argparse
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -24,6 +25,23 @@ augmentations = A.Compose([
     A.ColorJitter(p=0.2),
 ], bbox_params=A.BboxParams(format='coco', label_fields=['category_ids'], filter_invalid_bboxes=True))
 
+model_class_map = [
+    (lambda name: "gemma" in name, Gemma3ForConditionalGeneration),
+    (lambda name: "blip" in name, BlipForConditionalGeneration),
+    (lambda name: "kimi" in name, AutoModelForCausalLM),
+]
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Fine Tune Gemma3 for Object Detection")
+    parser.add_argument("--model", type=str, help="Model checkpoint identifier")
+    return parser.parse_args()
+
+def get_model_class(model_name):
+    model_name = model_name.lower()
+    for condition, model_class in model_class_map:
+        if condition(model_name):
+            return model_class
+    return AutoModelForSeq2SeqLM
 
 def get_dataloader(processor):
     logger.info("Fetching the dataset")
@@ -61,17 +79,30 @@ def train_model(model, optimizer, cfg, train_dataloader):
 
 
 if __name__ == "__main__":
+    args = parse_args()
     cfg = Configuration()
+    if args.model:
+        cfg.model_id = args.model
     processor = AutoProcessor.from_pretrained(cfg.model_id)
+    model_class = get_model_class(cfg.model_id)
     train_dataloader = get_dataloader(processor)
 
     logger.info("Getting model & turning only attention parameters to trainable")
-    model = Gemma3ForConditionalGeneration.from_pretrained(
-        cfg.model_id,
-        torch_dtype=cfg.dtype,
-        device_map="cpu",
-        attn_implementation="eager",
-    )
+
+    if "gemma" in cfg.model_id.lower():
+        model = model_class.from_pretrained(
+            cfg.model_id,
+            torch_dtype=cfg.dtype,
+            device_map="cpu",
+            attn_implementation="eager",
+        )
+    else:
+        model = model_class.from_pretrained(
+            cfg.model_id,
+            torch_dtype=cfg.dtype,
+            device_map="cpu",
+        )
+    
     for name, param in model.named_parameters():
         if "attn" in name:
             param.requires_grad = True
