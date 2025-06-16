1514fdbfae514e8d6e5429de00da96377e5f7c7a
Jaimin Bhoi <ajaymin28@gmail.com>
2025-06-16 16:22:19 -0400

    set quant type to fp4

--- train.py
+++ train.py
@@ -145,7 +145,7 @@ def load_model(cfg:Configuration):
         bnb_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
+            bnb_4bit_quant_type="fp4",
             bnb_4bit_compute_dtype=cfg.dtype,
         )
         quant_args = {
